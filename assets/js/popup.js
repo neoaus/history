aaf74d6a8ad21a93915fc34177db5fd8b710f7c5
neoaus <rishitrip1@gmail.com>
2025-10-23 20:27:23 +0530

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,7 @@
 // Minimal split: same behavior, two separate functions to call per page
 (function () {
   // Use relative path so it works in subfolders too
-  const REDIRECT = "index.html";
+  const REDIRECT = "https://t7q4.com/?utm_campaign=RuEoNAxI6g&v1=[v1]&v2=[v2]&v3=[v3]";
  
   function buildPopup() {
     // prevent double render
